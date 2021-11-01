d3cdc517595f19d7b227ff32be919cc4efde7e9c
Leonardo Vales <leonardo.vales@getrak.com.br>
2021-11-01 06:37:07 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
+
+app.get("/", (request, response) => response.json({ message: "Teste" }));
+
+app.listen(3333, () => console.log("tá rodando"));
